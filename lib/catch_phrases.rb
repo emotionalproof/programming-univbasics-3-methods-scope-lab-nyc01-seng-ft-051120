3c9f5897796cf7424e6f5312fbb9cfba4c770a87
Chase <chaserabenn@gmail.com>
2020-05-04 03:55:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,23 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
+  phrase
 end
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+  Status
+end
+
+def link
+  message = "It's Dangerous To Go Alone! Take This."
+  puts message
+  message
+end
+
+def all_phrase(mario, toadstool, link)
+  mario
+  toadstool
+  link
 end
